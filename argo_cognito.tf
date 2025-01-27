c65473d7c84b36b947492366474a1127b9279a63
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-01-27 00:55:46 -0500

    Update argo_cognito.tf

--- argo_cognito.tf
+++ argo_cognito.tf
@@ -4,13 +4,17 @@ resource "aws_cognito_user_pool_client" "auth" {
   name                = "${var.app_name}-client"
   user_pool_id        = aws_cognito_user_pool.auth.id
   generate_secret     = true
-  explicit_auth_flows = ["ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_SRP_AUTH"]
-  callback_urls       = ["https://${local.argo_domain}/auth/callback"]
-  logout_urls         = ["https://${local.argo_domain}/auth/logout"]
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+  callback_urls       = ["https://${local.argo_domain}/auth/callback","https://${local.argo_domain}/api/dex/callback"]
+  logout_urls         = ["https://${local.argo_domain}/auth/logout","https://${local.argo_domain}/api/dex/logout"]
 
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code"]
-  allowed_oauth_scopes                 = ["email", "phone", "openid"]
+  allowed_oauth_scopes                 = ["openid", "profile", "email"]
   supported_identity_providers         = ["COGNITO"]
 }
 resource "aws_cognito_identity_pool" "argo_identity_pool" {
@@ -25,11 +29,14 @@ resource "aws_cognito_identity_pool" "argo_identity_pool" {
 
 
 resource "aws_cognito_user_group" "argo" {
-  name         = "${var.app_name}-argo-group"
+  name         = "admin"
   user_pool_id = aws_cognito_user_pool.auth.id
   role_arn     = aws_iam_role.argo_authenticated_role.arn
 }
-
+resource "aws_cognito_user_group" "argo_readers" {
+  user_pool_id = aws_cognito_user_pool.auth.id
+  name         = "readonly"
+}
 
 
 resource "aws_iam_role" "argo_authenticated_role" {
